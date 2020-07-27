a83cccb43ffa809d7f0fba3c5225bc9c35596915
AtlanticAccent <Want.your.suitcase@gmail.com>
2020-07-27 20:34:23 +0100

    Initial basic layout and stub event handlers.

--- starsector_mod_manager/mmBase.cpp
+++ starsector_mod_manager/mmBase.cpp
@@ -1,4 +1,62 @@
 #include "mmBase.h"
 
 mmBase::mmBase() : wxFrame(nullptr, wxID_ANY, "Starsector Mod Manager") {
+    wxPanel* mainPane = new wxPanel(this);
+    wxBoxSizer* mainSizer = new wxBoxSizer(wxVERTICAL);
+    wxBoxSizer* topSizer = new wxBoxSizer(wxHORIZONTAL);
+    wxBoxSizer* bottomSizer = new wxBoxSizer(wxHORIZONTAL);
+
+    m_ctrl = new wxDataViewListCtrl(mainPane, MM_LISTITEMDATACHANGE);
+
+    m_ctrl->AppendToggleColumn(wxT("Enabled"));
+    m_ctrl->AppendTextColumn(wxT("Mod Name"));
+    m_ctrl->AppendTextColumn(wxT("Version Number"));
+    m_ctrl->AppendTextColumn(wxT("Last Updated"));
+
+    m_ctrl->Enable();
+
+    topSizer->Add(m_ctrl, 1, wxALL | wxEXPAND, 5);
+
+    wxBoxSizer* buttonSizer = new wxBoxSizer(wxVERTICAL);
+
+    wxButton* add = new wxButton(mainPane, MM_ADD, "Add Mod +");
+    wxButton* remove = new wxButton(mainPane, MM_REMOVE, "Remove Mod -");
+    wxButton* toggle = new wxButton(mainPane, MM_TOGGLEALL, "Toggle All");
+
+    buttonSizer->Add(add, 0, wxEXPAND | wxBOTTOM | wxALIGN_TOP, 5);
+    buttonSizer->Add(remove, 0, wxEXPAND | wxBOTTOM | wxALIGN_TOP, 5);
+    buttonSizer->Add(toggle, 0, wxEXPAND | wxBOTTOM | wxALIGN_TOP, 5);
+    topSizer->Add(buttonSizer, 0, wxALL, 5);
+
+    mainSizer->Add(topSizer, 2, wxEXPAND | wxBOTTOM, 5);
+    mainSizer->Add(bottomSizer, 1, wxRESERVE_SPACE_EVEN_IF_HIDDEN);
+
+    mainPane->SetSizer(mainSizer);
+}
+
+BEGIN_EVENT_TABLE(mmBase, wxFrame)
+    EVT_DATAVIEW_ITEM_VALUE_CHANGED(MM_LISTITEMDATACHANGE, mmBase::onListItemDataChange)
+END_EVENT_TABLE()
+
+void mmBase::onListContextMenuDisplay(wxCommandEvent& event) {
+}
+
+void mmBase::onAddModClick(wxCommandEvent& event) {
+}
+
+void mmBase::onRemoveModClick(wxCommandEvent& event) {
+}
+
+void mmBase::onToggleAllClick(wxCommandEvent& event) {
+}
+
+void mmBase::onListItemDataChange(wxDataViewEvent& event) {
+    int i = m_ctrl->ItemToRow(event.GetItem());
+    std::string string;
+    string.append(m_ctrl->GetTextValue(i, 1));
+    string.append(" ");
+    string.append(m_ctrl->GetToggleValue(i, 0) ? "Active" : "Inactive");
+    string.append("\n");
+    wxWindowPtr<wxDialog> dlg(new wxMessageDialog(this, string));
+    dlg->ShowModal();
 }
--- starsector_mod_manager/mmBase.h
+++ starsector_mod_manager/mmBase.h
@@ -1,8 +1,32 @@
 #pragma once
 
-#include "wx/wx.h"
+#include <wx/wx.h>
+#include <wx/dataview.h>
+#include <wx/button.h>
+#include <wx/dialog.h>
+#include <wx/windowptr.h>
+#include <wx/datetime.h>
 
 class mmBase : public wxFrame {
 public:
 	mmBase();
+
+	void onListContextMenuDisplay(wxCommandEvent& event);
+	void onAddModClick(wxCommandEvent& event);
+	void onRemoveModClick(wxCommandEvent& event);
+	void onToggleAllClick(wxCommandEvent& event);
+	void onListItemDataChange(wxDataViewEvent& event);
+
+protected:
+	DECLARE_EVENT_TABLE()
+
+private:
+	wxDataViewListCtrl* m_ctrl;
+};
+
+enum {
+	MM_ADD = wxID_HIGHEST + 1,
+	MM_REMOVE = wxID_HIGHEST + 2,
+	MM_TOGGLEALL = wxID_HIGHEST + 3,
+	MM_LISTITEMDATACHANGE = wxID_HIGHEST + 4
 };
