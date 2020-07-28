70984f56d6256c42cbab72d954af589ea22dc077
AtlanticAccent <Want.your.suitcase@gmail.com>
2020-07-28 18:55:55 +0100

    Created mmSettings class that will serve as the interface to changing the app's settings.

---
+++ starsector_mod_manager/mmSettings.cpp
@@ -0,0 +1,36 @@
+#include "mmSettings.h"
+
+mmSettings::mmSettings(wxWindow* parent) : wxFrame(parent, wxID_ANY, "Settings", wxDefaultPosition, wxSize(400, 300)) {
+	SetBackgroundColour(wxColour(*wxWHITE));
+	wxBoxSizer* mainSizer = new wxBoxSizer(wxHORIZONTAL);
+	wxBoxSizer* verticalSizer = new wxBoxSizer(wxVERTICAL);
+	wxBoxSizer* topSizer = new wxBoxSizer(wxHORIZONTAL);
+
+	wxStaticText* installDirLabel = new wxStaticText(this, wxID_ANY, "");
+	installDirLabel->SetLabelMarkup("<b>Install Directory:</b>");
+	installDir = new wxTextCtrl(this, wxID_ANY, "");
+	wxButton* installDirBrowser = new wxButton(this, MM_INSTALL_DIR_BROWSE, "Browse");
+
+	verticalSizer->Add(installDirLabel, 0, wxBOTTOM, 10);
+
+	topSizer->Add(installDir, 1, wxEXPAND, 0);
+	topSizer->Add(installDirBrowser, 0, wxBOTTOM | wxEXPAND, 0);
+
+	verticalSizer->Add(topSizer, 1, wxEXPAND, 0);
+
+	mainSizer->Add(verticalSizer, 1, wxALL | wxFIXED_MINSIZE, 10);
+
+	this->SetSizer(mainSizer);
+}
+
+BEGIN_EVENT_TABLE(mmSettings, wxFrame)
+	EVT_BUTTON(MM_INSTALL_DIR_BROWSE, mmSettings::onInstallDirectoryBrowse)
+END_EVENT_TABLE()
+
+void mmSettings::onInstallDirectoryBrowse(wxCommandEvent& event) {
+	wxDirDialog d = new wxDirDialog(this, "Choose a directory", "/", 0, wxDefaultPosition);
+
+	if (d.ShowModal() == wxID_OK) {
+		installDir->SetLabel(d.GetPath());
+	}
+}
---
+++ starsector_mod_manager/mmSettings.h
@@ -0,0 +1,27 @@
+#ifndef MM_SETTINGS
+#define MM_SETTINGS
+
+#include <wx/wx.h>
+#include <wx/button.h>
+#include <wx/dirdlg.h>
+
+#define SETTINGS_ID wxID_HIGHEST + 100
+
+class mmSettings : public wxFrame {
+public:
+	mmSettings(wxWindow* parent);
+
+	void onInstallDirectoryBrowse(wxCommandEvent& event);
+
+protected:
+	DECLARE_EVENT_TABLE()
+
+private:
+	wxTextCtrl* installDir;
+};
+
+enum {
+	MM_INSTALL_DIR_BROWSE = SETTINGS_ID + 1
+};
+
+#endif // !MM_SETTINGS
