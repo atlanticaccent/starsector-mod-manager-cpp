b9c34ef7304e856ca27f59ae5cbde723800e2072
AtlanticAccent <Want.your.suitcase@gmail.com>
2020-07-30 02:11:39 +0100

    Implemented selecting and saving the game's install directory.
    This includes persisting the selected directory value to the application's configuration file, as well as checking the validity of the input directory string, and checking for changes to values in the settings panel to dynamically enable and disable the Apply button.

--- starsector_mod_manager/mmSettings.cpp
+++ starsector_mod_manager/mmSettings.cpp
@@ -1,36 +1,66 @@
 #include "mmSettings.h"
 
-mmSettings::mmSettings(wxWindow* parent) : wxDialog(parent, wxID_ANY, "Settings", wxDefaultPosition, wxSize(400, 300)) {
+mmSettings::mmSettings(wxWindow* parent, mmConfig& config) : wxDialog(parent, wxID_ANY, "Settings", wxDefaultPosition, wxSize(400, 300)), config(config) {
 	SetBackgroundColour(wxColour(*wxWHITE));
-	wxBoxSizer* mainSizer = new wxBoxSizer(wxHORIZONTAL);
+	mainSizer = new wxBoxSizer(wxVERTICAL);
 	wxBoxSizer* verticalSizer = new wxBoxSizer(wxVERTICAL);
 	wxBoxSizer* topSizer = new wxBoxSizer(wxHORIZONTAL);
 
 	wxStaticText* installDirLabel = new wxStaticText(this, wxID_ANY, "");
 	installDirLabel->SetLabelMarkup("<b>Install Directory:</b>");
-	installDir = new wxTextCtrl(this, wxID_ANY, "");
+	installDir = new wxTextCtrl(this, MM_INSTALL_DIR, "");
 	wxButton* installDirBrowser = new wxButton(this, MM_INSTALL_DIR_BROWSE, "Browse");
 
-	verticalSizer->Add(installDirLabel, 0, wxBOTTOM, 10);
+	verticalSizer->Add(installDirLabel, 0, wxBOTTOM, 5);
 
-	topSizer->Add(installDir, 1, wxEXPAND, 0);
-	topSizer->Add(installDirBrowser, 0, wxBOTTOM | wxEXPAND, 0);
+	topSizer->Add(installDir, 1, wxEXPAND);
+	topSizer->Add(installDirBrowser, 0, wxEXPAND);
 
-	verticalSizer->Add(topSizer, 1, wxEXPAND, 0);
+	verticalSizer->Add(topSizer, 0, wxBOTTOM | wxEXPAND, 5);
+	verticalSizer->AddStretchSpacer();
+	verticalSizer->Add(CreateButtonSizer(wxOK | wxCANCEL | wxAPPLY), 0, wxEXPAND);
 
-	mainSizer->Add(verticalSizer, 1, wxALL | wxFIXED_MINSIZE, 10);
-
+	mainSizer->Add(verticalSizer, 1, wxALL | wxEXPAND, 10);
 	this->SetSizer(mainSizer);
+
+	installDir->ChangeValue(config["install_dir"].get<std::string>());
+	wxWindow::FindWindowById(wxID_APPLY, this)->Enable(false);
 }
 
 BEGIN_EVENT_TABLE(mmSettings, wxDialog)
 	EVT_BUTTON(MM_INSTALL_DIR_BROWSE, mmSettings::onInstallDirectoryBrowse)
+
+	EVT_TEXT(MM_INSTALL_DIR, mmSettings::onChange)
+
+	EVT_BUTTON(wxID_OK, mmSettings::onApply)
+	EVT_BUTTON(wxID_APPLY, mmSettings::onApply)
 END_EVENT_TABLE()
 
 void mmSettings::onInstallDirectoryBrowse(wxCommandEvent& event) {
 	wxDirDialog d(this, "Choose a directory", "/", 0, wxDefaultPosition);
 
 	if (d.ShowModal() == wxID_OK) {
-		installDir->SetLabel(d.GetPath());
+		installDir->SetValue(d.GetPath());
 	}
+}
+
+void mmSettings::onChange(wxCommandEvent& event) {
+	//wxMessageDialog(this, "firing").ShowModal();
+	wxWindow::FindWindowById(wxID_APPLY, this)->Enable(true);
+}
+
+void mmSettings::onApply(wxCommandEvent& event) {
+	if (std::filesystem::exists(installDir->GetValue().ToStdString())) {
+		config["install_dir"] = installDir->GetValue();
+
+		config.apply();
+		wxWindow::FindWindowById(wxID_APPLY, this)->Enable(false);
+	} else {
+		wxMessageDialog error(this, "Directory does not exist");
+		error.SetWindowStyle(wxICON_ERROR);
+		error.ShowModal();
+	}
+
+	//set apply inactive
+	event.Skip();
 }
--- starsector_mod_manager/mmSettings.h
+++ starsector_mod_manager/mmSettings.h
@@ -5,23 +5,33 @@
 #include <wx/button.h>
 #include <wx/dirdlg.h>
 
+#include "mmConfig.h"
+
 #define SETTINGS_ID wxID_HIGHEST + 100
 
 class mmSettings : public wxDialog {
 public:
-	mmSettings(wxWindow* parent);
+	mmSettings(wxWindow* parent, mmConfig& config);
 
 	void onInstallDirectoryBrowse(wxCommandEvent& event);
+
+	void onChange(wxCommandEvent& event);
+
+	void onApply(wxCommandEvent& event);
 
 protected:
 	DECLARE_EVENT_TABLE()
 
 private:
+	wxBoxSizer* mainSizer;
 	wxTextCtrl* installDir;
+
+	mmConfig& config;
 };
 
 enum {
-	MM_INSTALL_DIR_BROWSE = SETTINGS_ID + 1
+	MM_INSTALL_DIR_BROWSE = SETTINGS_ID + 1,
+	MM_INSTALL_DIR = SETTINGS_ID + 2
 };
 
 #endif // !MM_SETTINGS
